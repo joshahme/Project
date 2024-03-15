b6f95650d3f11429354bf53f18d66b54bbf02de4
joshahme <155593968+joshahme@users.noreply.github.com>
2024-03-15 10:51:43 -0400

    Add files via upload
     Sales Insights  is an R package and it is mainly designed towards comprehensive analysis and visualization of sales data. It provides a collection of functions so as to explore patterns, trends and performance metrics related to sales across different products and time periods. This package entails functions towards calculating total sales, analyzing average sales per month, conducting product performance analysis , generating visualizations(e.g.; bar plots, line plots, scatter plots) and there by performing statistical analysis(e.g.; correlation analysis, time series analysis) and developing customizable reports summarizing key insights.

---
+++ Descriprion-Project.r
@@ -0,0 +1,11 @@
+Package: SalesInsights
+Type: Package
+Title: Sales Analysis and Visualization
+Version: 0.1
+Author: joshah <joshah@usf.edu> [aut, cre]
+Maintainer: Your Name <your.email@example.com>
+Description: SalesInsights is an R package designed mainly towards comprehensive analysis and visualization of sales data. It provides a suite of functions  as to explore trends, patterns, and performance metrics related to sales across different products and time periods. The package includes functions towards calculating total sales, analyzing average sales per month, conducting product performance analysis, generating visualizations (e.g., bar plots, line plots, scatter plots), performing statistical analysis (e.g., correlation analysis, time series analysis), and generating customizable reports summarizing key insights.
+License: MIT
+Encoding: UTF-8
+LazyData: true
+Imports: ggplot2, dplyr, tidyr, ggthemes, knitr
